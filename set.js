e17d095c577cd7cfabe9b12c101a34ed8501a036
OVLMDBOT-2 <ovlmdbot@gmail.com>
2025-02-08 13:20:36 -0500

    Update set.js

--- set.js
+++ set.js
@@ -1,16 +1,16 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
-  MODE: process.env.MODE || "public",
+  PREFIXE: process.env.PREFIXE || ".",
+  NOM_OWNER: process.env.NOM_OWNER || "BOSS",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "50935548683",
+  MODE: process.env.MODE || "privé",
   MENU: process.env.MENU || "https://i.ibb.co/ynx9QcZ/image.jpg",
-  SESSION_ID: process.env.SESSION_ID || "ovl",
+  SESSION_ID: process.env.SESSION_ID || "Ovl-MD_w7eE13V0_SESSION-ID",
   LEVEL_UP: process.env.LEVEL_UP || "non",
-  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
-  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "OVL-MD",
+  STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "𝚩𝚵𝚪𝐋𝚰𝚴 ⸸𝑉𝑎𝑙ℎ𝑎𝑙𝑙𝑎⸸",
+  STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "𝚩𝚵𝚪𝐋𝚰𝚴 ⸸𝑉𝑎𝑙ℎ𝑎𝑙𝑙𝑎⸸",
   DATABASE: process.env.DATABASE || "postgresql://postgres.qnjvgxwyncnsbpfxwrbq:ovlmdmdpasse@aws-0-eu-central-1.pooler.supabase.com:6543/postgres",
-  ANTIDELETE: process.env.ANTIDELETE || "non",
+  ANTIDELETE: process.env.ANTIDELETE || "oui",
   RENDER_API_KEY: process.env.RENDER_API_KEY,
 };
